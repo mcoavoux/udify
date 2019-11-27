05195ae9fa5fcefb6f6eb7ddb2721c164f95a59d
m <maximin.coavoux@univ-grenoble-alpes.fr>
2019-11-27 14:41:16 +0100

    concat french data

--- scripts/concat_ud_data.sh
+++ scripts/concat_ud_data.sh
@@ -4,7 +4,11 @@ DATASET_DIR="data/ud-treebanks-v2.3"
 
 echo "Generating multilingual dataset..."
 
-mkdir -p "data/ud"
+mkdir -p "data/ud/french"
 mkdir -p "data/ud/multilingual"
 
 python concat_treebanks.py data/ud/multilingual --dataset_dir ${DATASET_DIR}
+
+
+python concat_treebanks.py data/ud/french --dataset_dir ${DATASET_DIR} --treebanks UD_French-GSD UD_French-ParTUT UD_French-Sequoia UD_French-Spoken
+
